98da21289b50f2994aa12cb78cf25ac0f60cd470
kacper <kacper.zyla97@gmail.com>
2018-11-03 15:26:33 +0100

    Deleted unwanted attribute added one hot encoding, updated attribute names

--- data_preparation.py
+++ data_preparation.py
@@ -1,21 +1,35 @@
 import csv
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder
 
 datasetpath = "Bike-Sharing-Dataset"
 
 file = open(datasetpath + "/day.csv")
 daydata = csv.reader(file, delimiter=',')
 
-row1 = next(daydata)
 
-attribute_names = row1[2:]
 X = []
 for row in daydata:
     X.append(row[2:])
 
-X = np.array(X).astype(float)
+X = np.array(X)
 
-print(attribute_names)
+XX = np.array(X)
+
+XX = np.delete(XX,range(1,5),1)
+XX = np.delete(XX,4,1)
+XX = np.delete(XX,range(6,8),1)
+
+cat_idx = [0, 2]
+
+enc = OneHotEncoder(sparse=False, categorical_features=cat_idx)
+X = enc.fit_transform(XX)
+
+
+attribute_names = ['spring', 'summer', 'fall', 'winter', 'workingday', 'cloudy', 'rainy', 'clear',
+                   'temp', 'hum', 'windspeed', 'cnt']
+
+
 
 
 
