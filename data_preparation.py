def87a8743b52e9fe329981d35018c41ed6f00c0
ciokos <wciokos@gmail.com>
2018-11-03 21:11:13 +0100

    ANN

--- data_preparation.py
+++ data_preparation.py
@@ -15,26 +15,14 @@ X = np.array(X)
 
 XX = np.array(X)
 
-XX = np.delete(XX,range(1,5),1)
-XX = np.delete(XX,4,1)
-XX = np.delete(XX,range(6,8),1)
+XX = np.delete(XX, range(1, 5), 1)
+XX = np.delete(XX, 4, 1)
+XX = np.delete(XX, range(6, 8), 1)
 
 cat_idx = [0, 2]
 
 enc = OneHotEncoder(sparse=False, categorical_features=cat_idx)
 X = enc.fit_transform(XX)
 
-
 attribute_names = ['spring', 'summer', 'fall', 'winter', 'workingday', 'cloudy', 'rainy', 'clear',
                    'temp', 'hum', 'windspeed', 'cnt']
-
-
-
-
-
-
-
-
-
-
-
